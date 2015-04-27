deaebfc8b2466eefcd8f2ce0650896b7f1dc9f7e
gmagogsfm <gmagogsfm@gmail.com>
2015-04-27 09:10:51 -0500

    Adding zxing qrcode library Able to generate qrcode from first name now

--- mobile/src/main/java/me/yanancao/qrnamecard/NameCard.java
+++ mobile/src/main/java/me/yanancao/qrnamecard/NameCard.java
@@ -1,15 +1,19 @@
 package me.yanancao.qrnamecard;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-import com.google.zxing.qrcode.QRCodeReader;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 
@@ -18,6 +22,7 @@ public class NameCard extends ActionBarActivity {
     public String TAG = "yanan_debug";
 
     public final void updateQR(View view){
+
         EditText firstNameTextField = (EditText) findViewById(R.id.firstName);
         EditText lastNameTextField = (EditText) findViewById(R.id.lastName);
         EditText emailTextField = (EditText) findViewById(R.id.email);
@@ -28,6 +33,33 @@ public class NameCard extends ActionBarActivity {
         Log.d(TAG,emailTextField.getText().toString());
         Log.d(TAG,phoneNumberTextField.getText().toString());
 
+
+        String qr_input = firstNameTextField.getText().toString();
+        QRCodeWriter writer = new QRCodeWriter();
+        try{
+            BitMatrix codeBitMatrix = writer.encode(qr_input, BarcodeFormat.QR_CODE, 800, 800);
+            Bitmap codeBitMap = toBitmap(codeBitMatrix);
+
+            ImageView qrcode = (ImageView) findViewById(R.id.qrCode);
+
+            qrcode.setImageBitmap(codeBitMap);
+        }
+        catch (WriterException e){
+            Log.d(TAG,"exception from qr code writer");
+        }
+
+    }
+
+    private Bitmap toBitmap(BitMatrix matrix){
+        int height = matrix.getHeight();
+        int width = matrix.getWidth();
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                bmp.setPixel(x, y, matrix.get(x,y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+        return bmp;
     }
 
     @Override
